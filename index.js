f927b48df7c6648069747f6655fb87c50c65a185
WessCoby <cobygiven@gmail.com>
2019-07-20 20:38:07 +0000

    Switched to using lowdb

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 #!/usr/bin/env node
-const db = require('./db'),
-    lolcat = require('lolcatjs'),
-    cowsay = require('cowsay'),
-    chalk = require('chalk'),
-    fetchQuote = require('./src/fetch_quote');
 
-    
-const { log } = console;
-
-lolcat.options.seed = Math.round(Math.random() * 1000);
-lolcat.options.colors = true;
+const fetchQuote = require('./src/fetch_quote'),
+    { log } = console;
 
 
-
-// display.stdout.on('data', data => log(data))
-// log(fetchQuote())
-
-// lolcat.fromString(process.stdout.write(cowsay.say({text: fetchQuote()})));
-
-// log(db.get('quotes').getRandomQuote().value());
-
-// lolcat.fromPipe(console.log(cowsay.say({text: fetchQuote()})))
+// Log fetched quote
+log(fetchQuote())
