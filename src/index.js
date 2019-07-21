767420d74fb777cda7b9d9a6a59334d6e040d264
WessCoby <cobygiven@gmail.com>
2019-07-21 18:08:32 +0000

    Fixed #2 lowdb issue. Now, globally install qotm wont have any permission issues.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 
-// Add quotes function
+const contributing = require('../contributing.json'),
+    db = require('./db');
 
+// Add quotes function
+contributing.quotes.forEach(quote => {
+    db.get('quotes')
+        .push(quote)
+        .write();
+
+        console.log(`Quote by ${quote.author} added to quotes database.`);
+});
