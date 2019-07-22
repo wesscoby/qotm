4fcf7ee4bd0a4a630e9ffb2b49cb82bfedd5a619
WessCoby <cobygiven@gmail.com>
2019-07-22 20:47:45 +0000

    Added commander package.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
    const QuotesDatabase = require('./db.json'),
+    program = require('commander'),
     { log } = console;
 
-(function fetchQuote(){
+const fetchQuote = () =>{
     // Get random number between 0 and QuotesDatabase.quotes.length
     const getRandomNumber = (maximumNumber = QuotesDatabase.quotes.length) =>
      Math.floor(Math.random() * maximumNumber);
@@ -11,4 +12,15 @@
      const fetchedQuote = QuotesDatabase.quotes[getRandomNumber()];
     // Log output
     log(`${fetchedQuote.text}\n\n -- ${fetchedQuote.author}`);
-})();
+};
+
+// Initialize program
+program
+    .version(require('./package.json').version)
+    .description('Display a random tech or programming quote');
+
+// Run Qotm
+program
+    .action(() => fetchQuote());
+
+program.parse(process.argv);
